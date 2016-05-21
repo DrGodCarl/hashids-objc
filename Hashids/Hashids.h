75249645e15b971e81d8b7e10f89c8d2cedfccbd
Carl D. Benson <carl@carldbenson.com>
2016-05-20 21:03:10 -0500

    Trying a different approach for a runnable, testable project.

--- Hashids/Hashids.h
+++ Hashids/Hashids.h
@@ -3,7 +3,7 @@
 //  Hashids
 //
 //  Created by Carl Benson on 5/20/16.
-//
+//  Copyright © 2016 Carl Benson. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
---
+++ Hashids/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hashids
+//
+//  Created by Carl Benson on 5/20/16.
+//  Copyright © 2016 Carl Benson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
