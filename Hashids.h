89ac7825a255caa9ad1230f97df7e7b5789cca7e
Carl D. Benson <carl@carldbenson.com>
2016-05-20 20:45:47 -0500

    Set up project.

---
+++ Hashids.h
@@ -0,0 +1,13 @@
+//
+//  Hashids.h
+//  Hashids
+//
+//  Created by Carl Benson on 5/20/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hashids : NSObject
+
+@end
---
+++ Hashids.m
@@ -0,0 +1,13 @@
+//
+//  Hashids.m
+//  Hashids
+//
+//  Created by Carl Benson on 5/20/16.
+//
+//
+
+#import "Hashids.h"
+
+@implementation Hashids
+
+@end
